45210f5b72e74c91aa66a3a7557169dc74f1e840
juliapiazzalunga <julia.piazzalunga@escola.pr.gov.br>
2025-10-10 08:01:21 -0300

    dsveagverqgrgr

--- perguntas.js
+++ perguntas.js
@@ -1,5 +1,15 @@
-criaCartao(
-    'programacao',
-    'o que é Python'
-    'o python é uma linguagem de programacao'
+criaCartão(
+    'Programação',
+    'O que é Python'
+    'O python é uma linguagem de programacao'
+)
+criaCartão(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da frança é paris'
+)
+criaCartão(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de codigo que executa alguma tarefa'
 )
